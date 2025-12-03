e4b2914a1e8b4cccdb0b4f597b61c2afe6683134
Wadson Junior <wadsonjunior2005@gmail.com>
2025-12-03 17:29:20 -0300

    configurações Prisma Client

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 import express = require("express");
+import { PrismaClient } from "../generated/prisma/client";
 
 const port = 3000;
 const app = express();
+const prisma = new PrismaClient();
 
 app.get("/movies", (req, res) => {
   res.send("Listagem de filmes");
