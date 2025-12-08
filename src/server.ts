53ccf90dd138088de70b25aa88c19f029ba8672a
Wadson Junior <wadsonjunior2005@gmail.com>
2025-12-08 15:01:39 -0300

    adicionado correção de filmes duplicados

--- src/server.ts
+++ src/server.ts
@@ -19,7 +19,18 @@ app.get("/movies", async (_, res) => {
 
 app.post("/movies", async (req, res) => {
   const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+
   try {
+    const movieWithSameTitle = await prisma.movie.findFirst({
+      where: { title: { equals: title, mode: "insensitive" } },
+    });
+
+    if (movieWithSameTitle) {
+      return res
+        .status(409)
+        .send({ message: "Já existe um filme cadastrado com esse título " });
+    }
+
     await prisma.movie.create({
       data: {
         title,
