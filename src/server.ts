b548d8a2364163463ea5aa75fc499dcab75d6f72
Wadson Junior <wadsonjunior2005@gmail.com>
2025-12-08 14:53:11 -0300

    adicionado: cadastro de filme, mensagem de error para falha no cadastro de filme

--- src/server.ts
+++ src/server.ts
@@ -4,15 +4,36 @@ import { prisma } from "./prisma";
 const port = 3000;
 const app = express();
 
+app.use(express.json());
+
 app.get("/movies", async (_, res) => {
   const movies = await prisma.movie.findMany({
     orderBy: { title: "asc" },
     include: {
       genres: true,
       languages: true,
-    }
+    },
   });
   res.json(movies);
+});
+
+app.post("/movies", async (req, res) => {
+  const { title, genre_id, language_id, oscar_count, release_date } = req.body;
+  try {
+    await prisma.movie.create({
+      data: {
+        title,
+        genre_id,
+        language_id,
+        oscar_count,
+        release_date: new Date(release_date),
+      },
+    });
+  } catch (error) {
+    return res.status(500).send({ message: "Falha ao cadastrar filme" });
+  }
+
+  res.status(201).send();
 });
 
 app.listen(port, () => {
